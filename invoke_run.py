7b0f78aa072a31e73ea9b4279d3e1c0c0c9fe0be
cclauss <cclauss@bluewin.ch>
2017-03-27 17:25:21 +0400

    Create invoke_run.py

---
+++ invoke_run.py
@@ -0,0 +1,49 @@
+class invoke(object):
+    def run(self, args, env):
+        def error(msg):
+            # fall through (exception and else case are handled the same way)
+            sys.stdout.write('%s\n' % msg)
+            return (502, {'error': 'The action did not return a dictionary.'})
+
+        try:
+            input = json.dumps(args)
+            p = subprocess.Popen(
+                [self.binary, input],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                env=env)
+        except Exception as e:
+            return error(e)
+
+        # run the process and wait until it completes.
+        # stdout/stderr will always be set because we passed PIPEs to Popen
+        (o, e) = p.communicate()
+
+        # stdout/stderr may be either text or bytes, depending on Python
+        # version.   In the latter case, decode to text.
+        if not isinstance(o, str):
+            o = o.decode('utf-8')
+        if not isinstance(e, str):
+            e = e.decode('utf-8')
+
+        # get the last line of stdout, even if empty
+        process_output_lines = o.strip().split('\n')
+        last_line = process_output_lines[-1]
+        for line in process_output_lines[:-1]:
+            sys.stdout.write('%s\n' % line)
+
+        if e:
+            sys.stderr.write(e)
+
+        try:
+            json_output = json.loads(last_line)
+            if isinstance(json_output, dict):
+                return (200, json_output)
+            else:
+                return error(last_line)
+        except Exception:
+            return error(last_line)
+
+i = invoke()
+print(i.run('snowy_version_py2.py'))
+print(i.run('snowy_version_py3.py'))
